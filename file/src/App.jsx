83959b7891f618b091731c473a8031751a5f315e
Anmol Kumar <anmolcricket888@gmail.com>
2024-03-08 21:31:16 +0530

    done

---
+++ file/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import Props001 from "./components/Props001"
+import MoviesList from "./components/MoviesList"
+
+function App() {
+  
+
+  return (
+    <>
+    <Props001/>
+    <MoviesList/>
+    </>
+  )
+}
+
+export default App
